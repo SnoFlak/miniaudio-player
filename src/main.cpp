97595886dd8dcda9c008d94115e366ee2ca62f97
Mathias Sletten <mathiasasletten@gmail.com>
2025-12-02 16:50:48 -0600

    basic dear imgui setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 #define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -12,26 +16,93 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "miniaudio-player", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 450, "miniaudio-player", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
+    int min_width = 500;
+    int min_height = 300;
+    int max_width = 800;
+    int max_height = 450;
+
+    glfwSetWindowSizeLimits(
+        window, 
+        min_width, 
+        min_height, 
+        max_width, 
+        max_height
+    );
+
     glfwMakeContextCurrent(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glViewport(0, 0, 800, 600);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 0.00f);
+    const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGuiStyle& style = ImGui::GetStyle();
 
     while(!glfwWindowShouldClose(window)) {
-        processInput(window);
-        glfwSwapBuffers(window);
+        ImVec2 sidebar_size = ImVec2(viewport->Size.x / 3 < 100.0f ? 100.0f : viewport->Size.x / 3, viewport->Size.y * 0.8 < 240.0f ? 240.0f : viewport->Size.y * 0.8);
+        ImVec2 visualizer_size = ImVec2(0.0f, viewport->Size.y * 0.8 < 240.0f ? 240.0f : viewport->Size.y * 0.8);
+        ImVec2 controlbar_size = ImVec2(0.0f, viewport->Size.y * 0.15 < 45.0f ? 45.0f : viewport->Size.y * 0.15 > 65.0f ? 65.0f : viewport->Size.y * 0.15);
         glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::Begin("Hello ImGui", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+
+        // if (ImGui::Button("Play", ImVec2(75.0f, 25.0f))) {
+        //     std::cout << "Button pressed" << std::endl;
+        // }
+        ImGui::BeginChild("Sidebar", sidebar_size, ImGuiChildFlags_Borders);
+        ImGui::Text("test text");
+        ImGui::EndChild();
+        ImGui::SameLine();
+        ImGui::BeginChild("Visualizer", visualizer_size, ImGuiChildFlags_Borders);
+        ImGui::Text("test text");
+        ImGui::EndChild();
+
+        ImGui::BeginChild("ControlBar", controlbar_size, ImGuiChildFlags_Borders);
+        ImGui::Text("test text");
+        ImGui::EndChild();
+
+        ImGui::End();
+        // ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
